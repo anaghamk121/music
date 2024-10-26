b5ad7dd2bd075a8d763d2301d44a89ef7e4d0276
Anaghamk00 <anaghadev00@gmail.com>
2024-10-26 15:41:31 +0530

    commit

---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const secondarycolor = Color(0xFFB71C1C);
+const primarycolor = Color(0xFF212121);
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:music/views/home.dart';
+import 'package:get/get.dart';
+import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      home: const Home(),
-        title: 'Beats',
-        theme: ThemeData(
-          fontFamily: "Inspiration",
-          appBarTheme:
-              const AppBarTheme(backgroundColor: Colors.transparent, elevation: 0),
-    textTheme: const TextTheme(
-    bodyLarge: TextStyle(fontFamily: "Inspiration"),
-    bodyMedium: TextStyle(fontFamily: "Inspiration"),
-
-    )));
-  }
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Beats",
+      initialRoute: AppPages.INITIAL,
+theme: ThemeData.dark(),
+      getPages: AppPages.routes,
+    ),
+  );
 }
